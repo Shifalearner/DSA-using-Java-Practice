f43e6231cadcc4ff86738b9e3151ff963550d3e5
Shifalearner <shifasayyad032@gmail.com>
2025-11-07 17:53:32 +0530

    first problem solved

---
+++ Day1_06_11_25.java
@@ -0,0 +1,14 @@
+public class Day1_06_11_25 {
+
+     public void check(int n){
+            if(n % 2 == 0){
+                System.out.println("even");
+            }else{
+                System.out.println("Odd");
+            }
+        }
+    public static void main(String[] args) {
+       Day1_06_11_25 d = new Day1_06_11_25();
+       d.check(13);
+    }
+}
