af88199355dbc4fb3e0be507afccf81d81af453e
Shifalearner <shifasayyad032@gmail.com>
2025-11-08 16:10:39 +0530

    Day 2 code completed

--- POD/Day2_07_11.java
+++ POD/Day2_07_11.java
@@ -1,5 +1,32 @@
 package POD;
 
+import java.util.Scanner;
+
 public class Day2_07_11 {
-    
+    public void print(int n){
+       
+        
+        for(int i = 1; i <= n; i++){
+      
+            if(i % 3 == 0 && i % 5 == 0 ){
+                System.out.println("FizzBuzz");
+            }else if(i % 3 == 0){
+                System.out.println("Fizz");
+            }else if(i % 5 == 0){
+                System.out.println("Buzz");
+            }else{
+                System.out.println(i);
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+         Scanner sc = new Scanner(System.in);
+         
+        System.out.println("Enter any number");
+        int n = sc.nextInt();
+        Day2_07_11 d = new Day2_07_11();
+        d.print(n);
+        sc.close();
+    }
 }
